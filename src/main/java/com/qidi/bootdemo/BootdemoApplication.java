c2b6c491ca4e3d05edfa87bad281f172306bae32
马启迪 <maqidi@meituan.com>
2018-07-20 19:03:17 +0800

    update

--- src/main/java/com/qidi/bootdemo/BootdemoApplication.java
+++ src/main/java/com/qidi/bootdemo/BootdemoApplication.java
@@ -3,6 +3,11 @@ package com.qidi.bootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class BootdemoApplication {
@@ -10,5 +15,17 @@ public class BootdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootdemoApplication.class, args);
 //		SpringApplicationBuilder()
+	}
+
+	@Bean
+	public ViewResolver myViewResoler(){
+		return new MyViewResoler();
+	}
+	private static class MyViewResoler implements ViewResolver{
+
+		@Override
+		public View resolveViewName(String s, Locale locale) throws Exception {
+			return null;
+		}
 	}
 }
