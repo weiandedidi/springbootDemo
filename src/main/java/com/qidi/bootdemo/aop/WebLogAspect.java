e3f7b21b0db0de388b89feae2bb8a91b52d6ffcb
马启迪 <maqidi@meituan.com>
2018-07-28 11:45:46 +0800

    aop

---
+++ src/main/java/com/qidi/bootdemo/aop/WebLogAspect.java
@@ -0,0 +1,32 @@
+package com.qidi.bootdemo.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qidi
+ * Date: 2018/7/28
+ * Time: 上午11:33
+ */
+@Aspect
+@Component
+public class WebLogAspect {
+    private Logger logger = LoggerFactory.getLogger(WebLogAspect.class);
+
+    @Around("execution(*com.qidi.bootdemo.service.user.UserService)")
+    public Object aroundInvoke(ProceedingJoinPoint point) throws Throwable {
+        logger.info("around 执行参数是：" + Arrays.asList(point.getArgs()));
+
+        Object object = point.proceed(point.getArgs());
+        logger.info("返回结果  " + object);
+        return object;
+
+    }
+}
