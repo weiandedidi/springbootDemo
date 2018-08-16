b6f35aac88c93cb81df04df208a15b37379d097d
马启迪 <maqidi@meituan.com>
2018-08-16 16:59:45 +0800

    update

--- src/main/java/com/qidi/bootdemo/config/SecurityConfig.java
+++ src/main/java/com/qidi/bootdemo/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.qidi.bootdemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * 继承 重写WebSecurityConfigurerAdapter
- *
+ *   .mvcMatchers("/hello").permitAll() 放权
  * User: qidi
  * Date: 2018/8/10
  * Time: 上午10:42
@@ -15,9 +17,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //放行的
+        //定制请求的授权规则
         http.authorizeRequests().mvcMatchers("/css/**", "/index").permitAll()
-        .mvcMatchers("/hello","/index").permitAll();
-        //不放行的
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error");
+        //url权限拦截
+
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 }
